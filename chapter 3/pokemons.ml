f228b12840969780f7d47d63cb5c3c3c2ddbd356
MatisseAD <matisse.martayan@gmail.com>
2026-01-02 11:16:57 +0100

    Add pokemon type definitions and effectiveness functions in OCaml

---
+++ chapter 3/pokemons.ml
@@ -0,0 +1,26 @@
+type ptype = TNormal | TFire | TWater
+
+type peff = ENormal | ENotVery | ESuper
+
+
+let mutltiplier_of_eff = function
+  | ENormal -> 1.
+  | ENotVery -> 0.5
+  | ESuper -> 2.0
+
+let eff t1 t2 = match t1, t2 with
+| TFire, TFire | TWater, TWater | TFire, TWater -> ENotVery
+| TWater, TFire -> ESuper
+| _ -> ENormal
+
+type pokemon = {
+  name : string;
+  hp : float;
+  ptype : ptype;
+}
+
+let matisse = {
+  name = "Matisse";
+  hp = 500.;
+  ptype = TWater
+}
--- chapter 3/variants.ml
+++ chapter 3/variants.ml
@@ -27,3 +27,4 @@ let center s =
   | Point p -> p
 
 
+
