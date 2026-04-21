1e2923351dda136d90fbda71aee18b33bffd779f
MatisseAD <matisse.martayan@gmail.com>
2026-04-21 15:36:46 +0200

    add some changements

--- Exercices/chapitre5.ml
+++ Exercices/chapitre5.ml
@@ -24,12 +24,56 @@ let rec maxABR (t : 'a arbre) =
   | Vide -> min_int
   | N (g, x, d) -> max (max x (maxABR g)) (max x (maxABR d))
 
-
 (** Exercice 5.4 *)
 
-let searchObjectinABR (t : 'a arbre) (x : 'a) =
-  let rec aux t ans =
-    match t with
-    | Vide -> aux t ans
-    | N (g,p,d) ->
-      if p = x then
+(**let searchObjectinABR (t : 'a arbre) (x : 'a) = let rec aux t ans = match t
+   with | Vide -> aux t ans | N (g,p,d) -> if p = x then**)
+
+(** Exercice 1 TD *)
+
+type arbre = Vide | Noeud of int * arbre list
+
+let rec taille (t : arbre) : int =
+  match t with Vide -> 0 | Noeud (_, l) -> 1 + autres_arbres l
+
+and autres_arbres l =
+  match l with [] -> 0 | x :: xs -> taille x + autres_arbres xs
+
+let t =
+  Noeud
+    ( 1,
+      [
+        Noeud (2, []); Noeud (3, [ Noeud (5, []); Noeud (6, []) ]); Noeud (4, []);
+      ] )
+
+let rec hauteur (t : arbre) : int =
+  match t with Vide -> -1 | Noeud (_, l) -> 1 + hauteurs_arbres l
+
+and hauteurs_arbres l =
+  match l with [] -> -1 | x :: xs -> max (hauteur x) (hauteurs_arbres xs)
+
+let rec contient (a : arbre) (x : int) : bool =
+  match a with
+  | Vide -> false
+  | Noeud (p, l) -> if p = x then true else false || autres_arbres_content l x
+
+and autres_arbres_content l x =
+  match l with
+  | [] -> false
+  | ar1 :: ar1s -> contient ar1 x || autres_arbres_content ar1s x
+
+let nb_occurrences (a : arbre) (x : int) : int =
+  let rec aux (a : arbre list) (x : int) (cpt : int) : int =
+    match a with
+    | [] -> cpt
+    | t :: ts -> (
+        match t with
+        | Vide -> aux ts x cpt
+        | Noeud (p, l) ->
+            if p = x then aux (l @ ts) x (cpt + 1) else aux (l @ ts) x cpt)
+  in
+  aux [ a ] x 0
+
+let chemin (a : arbre) (x : int) =
+  let rec aux (a : arbre) (x : int) (l : int list option) =
+    
